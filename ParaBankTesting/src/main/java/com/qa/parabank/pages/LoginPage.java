e2ea6f5531fec88080d38e36c7f51c73e70f091b
s-sn-s <savin.sn.shetty@gmail.com>
2025-09-02 23:42:21 +0530

    modified to handle dropdown

--- ParaBankTesting/src/main/java/com/qa/parabank/pages/LoginPage.java
+++ ParaBankTesting/src/main/java/com/qa/parabank/pages/LoginPage.java
@@ -1,16 +1,28 @@
 package com.qa.parabank.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class LoginPage {
 
 	public static void main(String[] params) {
 		WebDriver driver = new ChromeDriver();
 		
-		driver.get("https://parabank.parasoft.com");
+		driver.get("https://the-internet.herokuapp.com");
 		
 		System.out.println(driver.getCurrentUrl());
+		
+		driver.findElement(By.linkText("Dropdown")).click();
+		
+		System.out.println(driver.getCurrentUrl());
+		
+		Select select = new Select(driver.findElement(By.id("dropdown")));
+		
+		select.selectByVisibleText("Option 2");
+		
+		System.out.println(select.getFirstSelectedOption().getText());
 		
 		driver.quit();
 	}	
