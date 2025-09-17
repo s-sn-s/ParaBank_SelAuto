987c0257574d4a72505eb78137a817f067c666dc
s-sn-s <savin.sn.shetty@gmail.com>
2025-09-17 23:45:40 +0530

    practice file

--- ParaBankTesting/src/main/java/com/qa/parabank/pages/LoginPage.java
+++ ParaBankTesting/src/main/java/com/qa/parabank/pages/LoginPage.java
@@ -1,12 +1,16 @@
 package com.qa.parabank.pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
-
+	
 	public static void main(String[] params) {
 		WebDriver driver = new ChromeDriver();
 		
@@ -23,6 +27,16 @@ public class LoginPage {
 		select.selectByVisibleText("Option 2");
 		
 		System.out.println(select.getFirstSelectedOption().getText());
+		
+		FluentWait<WebDriver> fwait = new FluentWait<WebDriver>(driver).until(null);
+		fwait.pollingEvery(Duration.ofSeconds(5));
+		
+		WebDriverWait iwait = new WebDriverWait(driver, Duration.ofSeconds(5),Duration.ofSeconds(1));
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(1));
+		
+		System.out.println("");
 		
 		driver.quit();
 	}	
