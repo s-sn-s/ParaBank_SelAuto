84cfce70b464f0f256bbf71ddcb717928b284f64
s-sn-s <savin.sn.shetty@gmail.com>
2025-09-20 00:13:00 +0530

    interacting with the DOM

--- ParaBankTesting/src/main/java/com/qa/parabank/pages/LoginPage.java
+++ ParaBankTesting/src/main/java/com/qa/parabank/pages/LoginPage.java
@@ -3,15 +3,18 @@ package com.qa.parabank.pages;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
 	
-	public static void main(String[] params) {
+	static void herokaupRandomTest() {
 		WebDriver driver = new ChromeDriver();
 		
 		driver.get("https://the-internet.herokuapp.com");
@@ -28,8 +31,9 @@ public class LoginPage {
 		
 		System.out.println(select.getFirstSelectedOption().getText());
 		
-		FluentWait<WebDriver> fwait = new FluentWait<WebDriver>(driver).until(null);
-		fwait.pollingEvery(Duration.ofSeconds(5));
+//		FluentWait<WebDriver> fwait = new FluentWait<WebDriver>(driver)
+//				.until(ExpectedConditions.);
+//		fwait.pollingEvery(Duration.ofSeconds(5));
 		
 		WebDriverWait iwait = new WebDriverWait(driver, Duration.ofSeconds(5),Duration.ofSeconds(1));
 		
@@ -39,5 +43,54 @@ public class LoginPage {
 		System.out.println("");
 		
 		driver.quit();
+	}
+	
+	public static void main(String[] params) {
+		
+		WebDriver driver = new ChromeDriver();
+		try {
+			driver.get("https://parabank.parasoft.com");
+			String url = driver.getCurrentUrl();
+			System.out.println(url);
+			
+			//login page by locators
+			By unameBy = By.name("username");
+			By userRegPageLink = By.linkText("Register");
+			
+			String uname = "Ghost"; 
+			
+			WebElement unameElement =  driver.findElement(unameBy);
+			WebElement userRegLinkElement = driver.findElement(userRegPageLink);
+			
+			unameElement.sendKeys(uname);
+			
+			String expURL = userRegLinkElement.getAttribute("href");
+			
+			System.out.println("Expected landing URL " + expURL);
+			
+			userRegLinkElement.click();
+			
+//			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+			
+			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+			
+			wait.until(ExpectedConditions.urlContains(expURL));
+			
+			wait.until((ExpectedCondition<Boolean>) webDriver ->
+    			((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete")
+			);
+			
+//			wait.until((ExpectedCondition<Boolean>) driver -> 
+//						
+//					);
+			
+			String actURL = driver.getCurrentUrl();
+			
+			System.out.println("Actual landing URL " + actURL);
+			
+		}
+		finally {
+			driver.quit();
+		}
 	}	
 }
